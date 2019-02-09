4ed8503b12b9b1dfb3f6f4fe20214f2ef07d44ca
Aaron Kaczmarek <aaronmichaelmusic@gmail.com>
2019-02-09 17:05:43 -0500

    tweaks

--- src/cds.js
+++ src/cds.js
@@ -11,38 +11,38 @@ const googlePBDuration = require('google-protobuf/google/protobuf/duration_pb.js
 let store 
 
 function streamClusters(call, callback) {
-  console.log('stream clusters>>>')
+  // console.log('stream clusters>>>')
   call.on('data', function( request ) {
     // deconstruct incoming request message
-		const params = discoveryRequest( request )
-		// console.log(JSON.stringify( params, null, 2 ))
+    const params = discoveryRequest( request )
+    // console.log(JSON.stringify( params, null, 2 ))
 
-		// get stored data for request
-		const storedData = store.get( 'cds', params )
-		if ( !storedData ) {
-			console.log('NO DATA AVAILABLE')
-			return this.end()
-		}
+    // get stored data for request
+    const storedData = store.get( 'cds', params )
+    if ( !storedData ) {
+      console.log('NO DATA AVAILABLE')
+      return this.end()
+    }
 
-		// check for nonce to stop infinite updates
-		if ( params.response_nonce === storedData.nonce ) {
-			return this.end()
-		}
+    // check for nonce to stop infinite updates
+    if ( params.response_nonce === storedData.nonce ) {
+      return this.end()
+    }
 
-		// build discovery response
-		const response = new discovery.DiscoveryResponse()
-		response.setVersionInfo( 0 )
-		response.setTypeUrl( 'type.googleapis.com/envoy.api.v2.Cluster' )
+    // build discovery response
+    const response = new discovery.DiscoveryResponse()
+    response.setVersionInfo( 0 )
+    response.setTypeUrl( 'type.googleapis.com/envoy.api.v2.Cluster' )
     response.setNonce( storedData.nonce )
     
     // build resources to assign
-		const resourcesList = storedData.resourcesList.map( function ( dataResource ) {
+    const resourcesList = storedData.resourcesList.map( function ( dataResource ) {
       //console.log(JSON.stringify( dataResource, null, 2 ))
-			// for each resource, great a google protobuf Any buffer message
-			const any = new googlePBAny.Any()
+      // for each resource, great a google protobuf Any buffer message
+      const any = new googlePBAny.Any()
 
-			// create Cluster message
-			// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/cds.proto#cluster
+      // create Cluster message
+      // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/cds.proto#cluster
       const cluster = new cdsPB.Cluster()
       cluster.setName( dataResource.name )
 
@@ -61,65 +61,65 @@ function streamClusters(call, callback) {
       cluster.setLbPolicy( cdsPB.Cluster.LbPolicy[ dataResource.lb_policy ] )
       
       // create ClusterLoadAssignment message
-			// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/eds.proto#clusterloadassignment
-			const clusterLoadAssignment = new edsPB.ClusterLoadAssignment()
-			clusterLoadAssignment.setClusterName( dataResource.load_assignment.cluster_name )
+      // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/eds.proto#clusterloadassignment
+      const clusterLoadAssignment = new edsPB.ClusterLoadAssignment()
+      clusterLoadAssignment.setClusterName( dataResource.load_assignment.cluster_name )
 
-			// build endpoints to assign
-			const endpoints = dataResource.load_assignment.endpoints.map( function ( dataEndpoint ) {
-				// create LocalityLbEndpoints message
-				// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-localitylbendpoints
-				const localityLbEndpoints = new endpointPB.LocalityLbEndpoints()
+      // build endpoints to assign
+      const endpoints = dataResource.load_assignment.endpoints.map( function ( dataEndpoint ) {
+        // create LocalityLbEndpoints message
+        // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-localitylbendpoints
+        const localityLbEndpoints = new endpointPB.LocalityLbEndpoints()
 
-				// build lbendpoints to assign 
-				const lbEndpoints = dataEndpoint.lb_endpoints.map( function ( dataLbEndpoint ) {
-					// create LbEndpoint message
-					// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-lbendpoint
-					const lbEndpoint = new endpointPB.LbEndpoint()
-					// create Endpoint message 
-					// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-endpoint
-					const endpoint = new endpointPB.Endpoint()
-					// create Address message 
-					// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/core/address.proto#envoy-api-msg-core-address
-					const address = new addressPB.Address()
-					// create SocketAddress message 
-					// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/core/address.proto#envoy-api-msg-core-socketaddress
-					const socketAddress = new addressPB.SocketAddress()
-					// assign values to socket address
-					socketAddress.setAddress( dataLbEndpoint.endpoint.address.socket_address.address )
-					socketAddress.setPortValue( dataLbEndpoint.endpoint.address.socket_address.port_value )
-					// assign socket address to address
-					address.setSocketAddress( socketAddress )
-					// assign address to endpoint
-					endpoint.setAddress( address )
-					// assign endpoint to lb endpoint
-					lbEndpoint.setEndpoint( endpoint )
+        // build lbendpoints to assign 
+        const lbEndpoints = dataEndpoint.lb_endpoints.map( function ( dataLbEndpoint ) {
+          // create LbEndpoint message
+          // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-lbendpoint
+          const lbEndpoint = new endpointPB.LbEndpoint()
+          // create Endpoint message 
+          // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-endpoint
+          const endpoint = new endpointPB.Endpoint()
+          // create Address message 
+          // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/core/address.proto#envoy-api-msg-core-address
+          const address = new addressPB.Address()
+          // create SocketAddress message 
+          // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/core/address.proto#envoy-api-msg-core-socketaddress
+          const socketAddress = new addressPB.SocketAddress()
+          // assign values to socket address
+          socketAddress.setAddress( dataLbEndpoint.endpoint.address.socket_address.address )
+          socketAddress.setPortValue( dataLbEndpoint.endpoint.address.socket_address.port_value )
+          // assign socket address to address
+          address.setSocketAddress( socketAddress )
+          // assign address to endpoint
+          endpoint.setAddress( address )
+          // assign endpoint to lb endpoint
+          lbEndpoint.setEndpoint( endpoint )
 
-					return lbEndpoint
-				})
-				// assign lb endpoints to locality lb endpoint
-				localityLbEndpoints.setLbEndpointsList( lbEndpoints )
-	
-				return localityLbEndpoints
-			}) 
+          return lbEndpoint
+        })
+        // assign lb endpoints to locality lb endpoint
+        localityLbEndpoints.setLbEndpointsList( lbEndpoints )
+  
+        return localityLbEndpoints
+      }) 
 
-			// assign endpoints to cluster
-			clusterLoadAssignment.setEndpointsList( endpoints )
+      // assign endpoints to cluster
+      clusterLoadAssignment.setEndpointsList( endpoints )
       
       // assign clusterLoadAssignment
       cluster.setLoadAssignment( clusterLoadAssignment )
       
-			// pack listener message into any
-			any.pack( cluster.serializeBinary(), 'envoy.api.v2.Cluster')
+      // pack listener message into any
+      any.pack( cluster.serializeBinary(), 'envoy.api.v2.Cluster')
       
-			return any
-		})
+      return any
+    })
 
-		// assign resources to response
-		response.setResourcesList( resourcesList )
+    // assign resources to response
+    response.setResourcesList( resourcesList )
     
-		// write response
-		this.write(response)
+    // write response
+    this.write(response)
   })
 }
 
@@ -132,9 +132,9 @@ function fetchClusters(call, callback) {
 }
 
 exports.registerServices = function ( server, configStore ) {
-	store = configStore 
+  store = configStore 
 
-	server.addService(
+  server.addService(
     cdsServices.ClusterDiscoveryServiceService, 
     {
       streamClusters: streamClusters,
--- src/eds.js
+++ src/eds.js
@@ -10,91 +10,91 @@ const googlePBAny = require('google-protobuf/google/protobuf/any_pb.js')
 let store
 
 function streamEndpoints(call, callback) {
-	console.log('stream endpoints called')
-	call.on('data', function( request ) {
-		// deconstruct incoming request message
-		const params = discoveryRequest( request )
-		// get stored data for requested resource name 
-		const storedData = store.get( 'eds', params.resource_names[ 0 ])
+  console.log('stream endpoints called')
+  call.on('data', function( request ) {
+    // deconstruct incoming request message
+    const params = discoveryRequest( request )
+    // get stored data for requested resource name 
+    const storedData = store.get( 'eds', params.resource_names[ 0 ])
 
-		// build discovery response
-		const response = new discovery.DiscoveryResponse()
-		response.setVersionInfo( 0 )
-		response.setTypeUrl( 'type.googleapis.com/envoy.api.v2.ClusterLoadAssignment' )
+    // build discovery response
+    const response = new discovery.DiscoveryResponse()
+    response.setVersionInfo( 0 )
+    response.setTypeUrl( 'type.googleapis.com/envoy.api.v2.ClusterLoadAssignment' )
 
-		// build resources to assign
-		const resourcesList = storedData.map( function ( dataResource ) {
-			// for each resource, great a google protobuf Any buffer message
-			const any = new googlePBAny.Any()
+    // build resources to assign
+    const resourcesList = storedData.map( function ( dataResource ) {
+      // for each resource, great a google protobuf Any buffer message
+      const any = new googlePBAny.Any()
 
-			// create ClusterLoadAssignment message
-			// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/eds.proto#clusterloadassignment
-			const clusterLoadAssignment = new edsPB.ClusterLoadAssignment()
-			clusterLoadAssignment.setClusterName( dataResource.cluster_name )
+      // create ClusterLoadAssignment message
+      // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/eds.proto#clusterloadassignment
+      const clusterLoadAssignment = new edsPB.ClusterLoadAssignment()
+      clusterLoadAssignment.setClusterName( dataResource.cluster_name )
 
-			// build endpoints to assign
-			const endpoints = dataResource.endpoints.map( function ( dataEndpoint ) {
-				// create LocalityLbEndpoints message
-				// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-localitylbendpoints
-				const localityLbEndpoints = new endpointPB.LocalityLbEndpoints()
+      // build endpoints to assign
+      const endpoints = dataResource.endpoints.map( function ( dataEndpoint ) {
+        // create LocalityLbEndpoints message
+        // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-localitylbendpoints
+        const localityLbEndpoints = new endpointPB.LocalityLbEndpoints()
 
-				// build lbendpoints to assign 
-				const lbEndpoints = dataEndpoint.lb_endpoints.map( function ( dataLbEndpoint ) {
-					// create LbEndpoint message
-					// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-lbendpoint
-					const lbEndpoint = new endpointPB.LbEndpoint()
-					// create Endpoint message 
-					// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-endpoint
-					const endpoint = new endpointPB.Endpoint()
-					// create Address message 
-					// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/core/address.proto#envoy-api-msg-core-address
-					const address = new addressPB.Address()
-					// create SocketAddress message 
-					// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/core/address.proto#envoy-api-msg-core-socketaddress
-					const socketAddress = new addressPB.SocketAddress()
-					// assign values to socket address
-					socketAddress.setAddress( dataLbEndpoint.endpoint.address.socket_address.address )
-					socketAddress.setPortValue( dataLbEndpoint.endpoint.address.socket_address.port_value )
-					// assign socket address to address
-					address.setSocketAddress( socketAddress )
-					// assign address to endpoint
-					endpoint.setAddress( address )
-					// assign endpoint to lb endpoint
-					lbEndpoint.setEndpoint( endpoint )
+        // build lbendpoints to assign 
+        const lbEndpoints = dataEndpoint.lb_endpoints.map( function ( dataLbEndpoint ) {
+          // create LbEndpoint message
+          // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-lbendpoint
+          const lbEndpoint = new endpointPB.LbEndpoint()
+          // create Endpoint message 
+          // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/endpoint/endpoint.proto#envoy-api-msg-endpoint-endpoint
+          const endpoint = new endpointPB.Endpoint()
+          // create Address message 
+          // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/core/address.proto#envoy-api-msg-core-address
+          const address = new addressPB.Address()
+          // create SocketAddress message 
+          // https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/core/address.proto#envoy-api-msg-core-socketaddress
+          const socketAddress = new addressPB.SocketAddress()
+          // assign values to socket address
+          socketAddress.setAddress( dataLbEndpoint.endpoint.address.socket_address.address )
+          socketAddress.setPortValue( dataLbEndpoint.endpoint.address.socket_address.port_value )
+          // assign socket address to address
+          address.setSocketAddress( socketAddress )
+          // assign address to endpoint
+          endpoint.setAddress( address )
+          // assign endpoint to lb endpoint
+          lbEndpoint.setEndpoint( endpoint )
 
-					return lbEndpoint
-				})
-				// assign lb endpoints to locality lb endpoint
-				localityLbEndpoints.setLbEndpointsList( lbEndpoints )
-	
-				return localityLbEndpoints
-			}) 
+          return lbEndpoint
+        })
+        // assign lb endpoints to locality lb endpoint
+        localityLbEndpoints.setLbEndpointsList( lbEndpoints )
+  
+        return localityLbEndpoints
+      }) 
 
-			// assign endpoints to cluster
-			clusterLoadAssignment.setEndpointsList( endpoints )
+      // assign endpoints to cluster
+      clusterLoadAssignment.setEndpointsList( endpoints )
 
-			// pack cluster load assignment message into any
-			any.pack( clusterLoadAssignment.serializeBinary(), 'envoy.api.v2.ClusterLoadAssignment')
+      // pack cluster load assignment message into any
+      any.pack( clusterLoadAssignment.serializeBinary(), 'envoy.api.v2.ClusterLoadAssignment')
 
-			return any
-		})
+      return any
+    })
 
-		// assign resources to response
-		response.setResourcesList( resourcesList )
+    // assign resources to response
+    response.setResourcesList( resourcesList )
 
-		// write response
-		this.write(response)
-	})
+    // write response
+    this.write(response)
+  })
 }
 
 function fetchEndpoints(call, callback) {
-	console.log('stream endpoints called')
+  console.log('stream endpoints called')
 }
 
 exports.registerServices = function ( server, configStore ) {
-	store = configStore 
+  store = configStore 
 
-	server.addService(
+  server.addService(
     edsServices.EndpointDiscoveryServiceService, 
     {
       streamEndpoints: streamEndpoints,
